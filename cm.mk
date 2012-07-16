34dd3cf372e7c40473aba2497b173f8c9f609692
Nick Reuter <nreuter85@gmail.com>
2012-07-16 12:36:23 -0500

    Fix sensors and lights for JB build
    Change-Id: Ia983f1776e201fd7a39df6f454e8c3ee1bb2cc31

--- cm.mk
+++ cm.mk
@@ -1,14 +1,19 @@
-$(call inherit-product, device/htc/glacier/full_glacier.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_RELEASE_NAME := MT4G
+PRODUCT_RELEASE_NAME := glacier
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit device configuration
+$(call inherit-product, device/htc/glacier/full_glacier.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_FINGERPRINT=htc_europe/htc_primou/primou:4.0.3/IML74K/39988.1:user/release-keys PRIVATE_BUILD_DESC="2.19.531.1 CL95282 release-keys"
-
-PRODUCT_NAME := cm_glacier
+# Device naming
 PRODUCT_DEVICE := glacier
+PRODUCT_NAME := cm_glacier
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC Glacier
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
